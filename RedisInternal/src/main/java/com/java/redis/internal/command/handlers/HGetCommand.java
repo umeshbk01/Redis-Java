3bd8c92838fdd7425caa587fb29e41f2e749cc32
umeshbk01 <52484698+umeshbk01@users.noreply.github.com>
2025-06-11 17:34:16 +0000

    updating Command handlers

--- RedisInternal/src/main/java/com/java/redis/internal/command/handlers/HGetCommand.java
+++ RedisInternal/src/main/java/com/java/redis/internal/command/handlers/HGetCommand.java
@@ -4,7 +4,11 @@ import com.java.redis.internal.command.CommandHandler;
 import com.java.redis.internal.datastore.DataStore;
 import com.java.redis.internal.protocol.Command;
 import com.java.redis.internal.protocol.RedisReply;
-
+/**
+ * This class handles the logic for the Redis HGET command, 
+ * retrieving a field from a hash and returning the appropriate
+ *  Redis protocol reply.
+ */
 public class HGetCommand implements CommandHandler {
     private final DataStore store;
     public HGetCommand(DataStore store) { this.store = store; }
