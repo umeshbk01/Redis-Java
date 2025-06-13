3e3a0d7cb7f8f13d8cbeaf4bc19e106ab4843cbe
umeshbk01 <52484698+umeshbk01@users.noreply.github.com>
2025-06-13 15:33:02 +0000

    adding netty server

---
+++ RedisInternal/src/main/java/com/java/redis/internal/server/NettyServer.java
@@ -0,0 +1,59 @@
+package com.java.redis.internal.server;
+
+import com.java.redis.internal.command.CommandExecutor;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class NettyServer {
+    private final int port;
+    private final CommandExecutor commandExecutor;
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+    private Channel serverChannel;
+
+    public NettyServer(int port, CommandExecutor commandExecutor) {
+        this.port = port;
+        this.commandExecutor = commandExecutor;
+    }
+    
+    public void start() throws InterruptedException {
+        // Initialize and start the Netty server components here
+        // This is where you would set up the bootstrap, channel options, etc.
+        bossGroup = new NioEventLoopGroup(1);
+        workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            // Initialize the channel pipeline with handlers
+                            ch.pipeline().addLast(new RedisServerHandler(commandExecutor));
+                        }
+                    })
+                    .childOption(ChannelOption.SO_KEEPALIVE, true);
+        ChannelFuture future = bootstrap.bind(port).sync();
+        serverChannel = future.channel();
+        System.out.println("Server started on port " + port);
+        serverChannel.closeFuture().sync();
+        
+    }
+    public void stop() throws InterruptedException {
+        if (serverChannel != null) {
+            serverChannel.close();
+        }
+        if(workerGroup != null){
+            workerGroup.shutdownGracefully();
+        }
+        if(bossGroup != null){
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ RedisInternal/src/main/java/com/java/redis/internal/server/RedisServerHandler.java
@@ -0,0 +1,39 @@
+package com.java.redis.internal.server;
+
+import java.io.InputStream;
+import java.util.concurrent.Executors;
+
+import com.java.redis.internal.command.CommandExecutor;
+import com.java.redis.internal.protocol.Command;
+import com.java.redis.internal.protocol.RedisReply;
+import com.java.redis.internal.protocol.RespParser;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class RedisServerHandler extends ChannelInboundHandlerAdapter{
+    private final CommandExecutor commandExecutor;
+    public RedisServerHandler(CommandExecutor commandExecutor) {
+        this.commandExecutor = commandExecutor;
+    }
+    @Override
+    public void channelRead(io.netty.channel.ChannelHandlerContext ctx, Object msg) throws Exception {
+        var buffer = (io.netty.buffer.ByteBuf) msg;
+        InputStream in = new io.netty.buffer.ByteBufInputStream(buffer);
+        RespParser parser = new RespParser(in);
+        try{
+            Command cmd = parser.parse();
+            Executors.newThreadPerTaskExecutor(Thread.ofVirtual())
+                .execute(() -> {
+                    RedisReply reply = commandExecutor.execute(cmd);
+                    ctx.writeAndFlush(Unpooled.copiedBuffer(reply.toBytes()));
+                });
+        }catch(Exception e) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(RedisReply.error(e.getMessage()).toBytes()));
+        }finally{
+            buffer.release();
+        }
+    }
+    @Override public void exceptionCaught(ChannelHandlerContext ctx,Throwable t){ctx.close();}
+}
